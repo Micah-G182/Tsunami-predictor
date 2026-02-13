d3d09b0d65c5f175b75b5914e6986446632325bd
Micah-G182 <mgetare24@gmail.com>
2026-02-13 23:54:53 +0300

    Update and rename EDA.py to eDA.py

--- eDA.py
+++ eDA.py
@@ -7,4 +7,5 @@ df.head()
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 df[['magnitude','cdi','mmi','sig','nst','dmin','gap','depth','latitude','longitude','Year','Month']]= scaler.fit_transform(df[['magnitude','cdi','mmi','sig','nst','dmin','gap','depth','latitude','longitude','Year','Month']])
+
 df.to_csv("earthquake_data_tsunami_cleaned3.csv")
