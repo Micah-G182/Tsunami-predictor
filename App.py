cbdf1c778674114c34c6ae7bcbde1ab7f01225a9
Micah-G182 <mgetare24@gmail.com>
2026-02-22 06:59:02 +0300

    Refactor imports and enhance styling in App.py

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as t
 import pandas as pd
 import pickle
 import numpy as np
@@ -128,4 +128,5 @@ st.write("______________________________________________________________________
 st.write("Model accuracy score = 93.0%")
 st.write("Model Engineers: Georgina and Peninah, 2026 Science and Technology Fair.")
 st.write("Thank you.")
+
 
